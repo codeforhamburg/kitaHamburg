60927bf889a9fbaa1f131bd6c6aa534a6ca6c43b
tike <timflex@gmx.de>
2014-12-19 18:13:44 +0100

    pulled all functions into app Object to keep global namespace clean

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -6,7 +6,47 @@ var app = {
     currentGEOJSONLayer: undefined,
 };
 
-function getSliderStates(){
+
+// generates the html to shown as the popup of each marker
+// see: http://leafletjs.com/reference.html#geojson (options: pointToLayer)
+// TODO: better styling, add css classes and so on...
+app.generatePopup = function (kita){
+    var tmpl = '<div class="container">';
+
+    if (kita.website !== undefined){
+        tmpl += '<a href="' + kita.website + '" target="blank">' + kita.name + '</a></br>';
+    } else{
+        tmpl +=  '<b>' + kita.name + '</b></br>';
+    }
+
+    tmpl += '<b>Träger</b>: ' + kita.operator + '</br>';
+
+    if (kita.email !== undefined && kita.contact !== undefined){
+        tmpl += '<b>Ansprechpartner</b>: <a href="mailto:' + kita.email + '">' + kita.contact + '</a>';
+    }
+    if (kita.phone !== undefined) {
+        tmpl += ' (' + kita.phone + ')';
+    }
+
+    tmpl += '</div>';
+    return tmpl;
+}
+
+// generates and styles a marker to be used to represent a feature
+// see: http://leafletjs.com/reference.html#marker
+// and: http://leafletjs.com/reference.html#icon
+// TODO: add custom icons? 
+app.generateMarker = function (feature, latlng){
+    return new L.marker(latlng, {
+        title: feature.properties.name,
+        alt: feature.properties.name,
+        opacity: 0.8,
+        riseOnHover: true,
+        riseOffset: 250
+    });
+}
+
+app.getSliderStates = function (){
     var currentOptions = {};
     $('input.slider').each(function(){
         var key = $(this).attr('id');
@@ -27,7 +67,7 @@ function getSliderStates(){
 }
 
 
-function updateMap(data){
+app.updateMap = function (data){
     var kitas = data || [];
     if (app.currentGEOJSONLayer !== undefined){
         app.map.removeLayer(app.currentGEOJSONLayer);
@@ -39,20 +79,20 @@ function updateMap(data){
     
     app.currentGEOJSONLayer = L.geoJson(GeoJSON, {
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(generatePopup(feature.properties));
+            layer.bindPopup(app.generatePopup(feature.properties));
         }
     }).addTo(app.map);
 }
 
-function searchKitas(){
-    var query = getSliderStates();
+app.searchKitas = function(){
+    var query = app.getSliderStates();
     $.ajax({
         type: 'POST',
         url: '/cgi-bin/kitas/',
         data: JSON.stringify(query),
         contentType: 'application/json',
         dataType: 'json',
-        success: updateMap,
+        success: app.updateMap,
         error: function (err){ console.log('error', err);},
     });
 }
@@ -93,12 +133,12 @@ $(document).ready(function() {
                 if (! $(checkboxSelector).prop('checked')){
                     $(checkboxSelector).prop('checked', true);
                 }
-                searchKitas();
+                app.searchKitas();
             });
         });
     
     $('input.filterSelect').click(function(){
-        searchKitas();
+        app.searchKitas();
     });
 });
 
